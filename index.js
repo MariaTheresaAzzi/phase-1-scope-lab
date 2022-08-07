a6936038497b8c0713ccdc0ed7823fb9382d1890
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-08-07 21:28:40 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+//console.log(customerName)
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+
+var bestCustomer = undefined;
+function setBestCustomer() {
+    return bestCustomer = "not bob";
+}
+
+
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "lisa";
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "jen";
+}
